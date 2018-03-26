2100425def72ee59ef31628076d470dc63278d48
mrmello <mrmello92@hotmail.com>
2018-03-26 00:08:11 -0300

    changing products includer

--- src/components/includer/components/includer-product/includer-product.js
+++ src/components/includer/components/includer-product/includer-product.js
@@ -22,14 +22,7 @@ export default class IncluderProduct extends Component {
               <Form.Group>
                 <Form.Field control={Input} label='Nome' placeholder='Nome' width={6}/>
                 <Form.Field control={Input} label='Preço' placeholder='Preço' width={4}/>
-                <Form.Field control={Select} label='Tamanho' options={options} placeholder='Tamanho' width={5}/>
-              </Form.Group>
-              <Form.Group>
                 <Form.Field control={Select} label='Categoria' options={options} placeholder='Categoria' width={5}/>
-                <Form.Field control={Select} label='Recheio' options={options} placeholder='Recheio' width={5}/>
-              </Form.Group>
-              <Form.Group inline>
-                <Form.Field control={Checkbox} label='Ativo' />
               </Form.Group>
             </Form>
           </Modal.Content>
