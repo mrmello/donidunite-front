989cd0a4086508fe02460f5b11d4e0e2a5c9d5f5
Marcos Mello <mrmello92@hotmail.com>
2018-06-03 18:16:37 -0300

    heroku endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import endpoints from './endpoints';
 
-//const URL = "https://donidunite-back.herokuapp.com";
-const URL = "http://localhost:3100";
+const URL = "https://donidunite-back.herokuapp.com";
+//const URL = "http://localhost:3100";
 
 export default {
     fetchProducts:    ()  => axios.get(`${URL}${endpoints.PRODUCTS}`),
