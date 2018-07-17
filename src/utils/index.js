5342b5927cbdb68a5758d1685fbd7d07e275f102
Marcos Mello <mrmello92@hotmail.com>
2018-07-17 16:42:49 -0300

    date format

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,8 @@
 export const formatDateToPicker = (date) => {
-  let d = new Date(date)
-  if (!date) d = new Date()
-
-  d = new Date( d.getTime() - d.getTimezoneOffset() * -30000 );
-  let month = '' + (d.getMonth() + 1),
-  day = '' + d.getDate(),
-  year = d.getFullYear()
+  let d = date ? new Date(date) : new Date()
+  let month = '' + (d.getUTCMonth() + 1),
+  day = '' + d.getUTCDate(),
+  year = d.getUTCFullYear()
 
   if (month.length < 2) month = '0' + month
   if (day.length < 2) day = '0' + day
@@ -15,10 +12,9 @@ export const formatDateToPicker = (date) => {
 
 export const formatDateToDsiplay = (date) => {
   let d = new Date(date)
-  var timeZoneDate = new Date( d.getTime() - d.getTimezoneOffset() * -30000 );
-  let month = '' + (timeZoneDate.getMonth() + 1),
-  day = '' + timeZoneDate.getDate(),
-  year = timeZoneDate.getFullYear()
+  let month = '' + (d.getUTCMonth() + 1),
+  day = '' + d.getUTCDate(),
+  year = d.getUTCFullYear()
 
   if (month.length < 2) month = '0' + month
   if (day.length < 2) day = '0' + day
